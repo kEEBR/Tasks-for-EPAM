b54128acc5ba7b41e11d0a9fd4723bc7ca6d02e2
AndreyT <and6407@mail.ru>
2020-02-04 18:11:38 +0400

    Task #1 for EPAM SYS C#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Console.WriteLine("Введите семь чисел");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0}-е число: ", i + 1);
+                array[i] = Int32.Parse(Console.ReadLine());
+            }
+            int temp;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Результат сортировки: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
