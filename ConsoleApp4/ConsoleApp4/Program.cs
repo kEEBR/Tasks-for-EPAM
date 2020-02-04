262e7a32702ed8e7d239ad385df487c6908b426f
AndreyT <and6407@mail.ru>
2020-02-04 18:22:07 +0400

    Task #4 for EPAM SYS C#

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static long Factorial(int n)
+        {
+            long fact = 1;
+            if (n != 0)
+            {
+                for (int i = 2; i <= n; i++)
+                {
+                    fact *= i;
+                }
+            }
+            return fact;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int n = Int32.Parse(Console.ReadLine());
+            if (n >= 0)
+            {
+                Console.WriteLine($"Факториал числа {n}: {Factorial(n)}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введено отрицательное число!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
